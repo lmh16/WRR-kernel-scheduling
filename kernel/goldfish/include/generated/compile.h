dddfe5622fe87a4185d14bf6ad08bed1f1536c60
lmh16 <limuhan@sjtu.edu.cn>
2018-06-14 06:39:54 -0700

    new initial

---
+++ kernel/goldfish/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Wed Jun 13 10:13:57 PDT 2018"
+#define LINUX_COMPILE_BY "limh"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.9 20150123 (prerelease) (GCC) "
